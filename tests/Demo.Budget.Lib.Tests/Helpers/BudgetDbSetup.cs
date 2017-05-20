a6b6bbedd3f2502eb7adec3cf4a925f0d5c89aba
Miguel Veloso <mvelosop@gmail.com>
2017-05-20 09:41:12 +0100

    Removed generic test helpers classes

--- tests/Demo.Budget.Lib.Tests/Helpers/BudgetDbSetup.cs
+++ tests/Demo.Budget.Lib.Tests/Helpers/BudgetDbSetup.cs
@@ -1,10 +1,9 @@
 ﻿using Demo.Budget.Lib.Data;
-using Domion.FluentAssertions.Helpers;
 using Microsoft.EntityFrameworkCore;
 
 namespace Demo.Budget.Lib.Tests.Helpers
 {
-    public class BudgetDbSetup : BaseDbSetup<BudgetDbContext>
+    public class BudgetDbSetup
     {
         private static string _connectionString = "Data Source=localhost;Initial Catalog=Demo.Budget.Lib.Tests;Integrated Security=SSPI;MultipleActiveResultSets=true";
         private static DbContextOptions<BudgetDbContext> _options;
@@ -23,10 +22,17 @@ namespace Demo.Budget.Lib.Tests.Helpers
             SetupDatabase();
         }
 
-        public override BudgetDbContext GetDbContext()
+        public BudgetDbContext GetDbContext()
         {
             return new BudgetDbContext(_options);
         }
 
+        private void SetupDatabase()
+        {
+            using (var dbContext = GetDbContext())
+            {
+                dbContext.Database.Migrate();
+            }
+        }
     }
 }
