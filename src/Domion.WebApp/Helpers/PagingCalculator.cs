88c43d024f1c4f20627a6c219ee6600a841c3869
Miguel Veloso <mvelosop@gmail.com>
2017-07-07 11:52:08 +0100

    Added PagingCalculator

---
+++ src/Domion.WebApp/Helpers/PagingCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Domion.WebApp.Helpers
+{
+    public class PagingCalculator
+    {
+        private const int DefaultPageSize = 10;
+
+        public PagingCalculator(int itemCount, int? pageNumber, int? pageSize)
+        {
+            if (itemCount < 0) throw new ArgumentOutOfRangeException("Must be > 0", nameof(itemCount));
+
+            Page = pageNumber ?? 1;
+            PageSize = pageSize ?? DefaultPageSize;
+            PageCount = itemCount / PageSize + (itemCount % PageSize > 0 ? 1 : 0);
+
+            if (PageCount == 0) Page = 1;
+
+            if (PageCount > 0 & Page > PageCount) Page = PageCount;
+
+            ItemCount = itemCount;
+
+            if (Page < 0) Page = 1;
+
+            OutOfRange = (pageNumber.HasValue & Page != pageNumber);
+        }
+
+        public int ItemCount { get; private set; }
+
+        public bool OutOfRange { get; private set; }
+
+        public int Page { get; private set; }
+
+        public int PageCount { get; private set; }
+
+        public int PageSize { get; private set; }
+
+        public int Skip => (Page - 1) * PageSize;
+
+        public int Take => PageSize;
+    }
+}
