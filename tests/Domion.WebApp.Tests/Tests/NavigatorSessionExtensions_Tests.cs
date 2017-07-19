d80633d065c0d66db99314f49a2fe85f567fb9b4
Miguel Veloso <mvelosop@gmail.com>
2017-07-19 08:25:00 +0100

    NavigatorSessionExtensions test passed

--- tests/Domion.WebApp.Tests/Tests/NavigatorSessionExtensions_Tests.cs
+++ tests/Domion.WebApp.Tests/Tests/NavigatorSessionExtensions_Tests.cs
@@ -11,19 +11,42 @@ namespace Domion.WebApp.Tests.Tests
     public class NavigatorSessionExtensions_Tests
     {
         [Fact]
-        public void SaveRouteValues_AddsRouteAndValues()
+        public void SaveRouteValues_AddsRoute_WhenNoParams()
+        {
+            // Arrange ---------------------------
+
+            RouteData routeData = CreateRouteData("Index", "Tenants");
+
+            var expectedObject = new { controller = "Tenants", action = "Index" };
+
+            ISession session = new TestSession();
+
+            RouteValueDictionary expected = new RouteValueDictionary(expectedObject);
+
+            // Act -------------------------------
+
+            session.SaveRouteValues(routeData);
+
+            RouteValueDictionary result = session.GetRouteValues(routeData);
+
+            // Assert ----------------------------
+
+            result.ShouldBeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void SaveRouteValues_AddsRouteAndParams_WhenNewRoute()
         {
             // Arrange ---------------------------
 
             RouteData routeData = CreateRouteData("Index", "Tenants");
 
             var paramsObject = new { p = 2, ps = 3 };
+            var expectedObject = new { controller = "Tenants", action = "Index", p = 2, ps = 3 };
+
+            RouteValueDictionary expected = new RouteValueDictionary(expectedObject);
 
             ISession session = new TestSession();
-
-            var routeObject = new { controller = "Tenants", action = "Index", p = 2, ps = 3 };
-
-            RouteValueDictionary expected = new RouteValueDictionary(routeObject);
 
             // Act -------------------------------
 
@@ -36,9 +59,34 @@ namespace Domion.WebApp.Tests.Tests
             result.ShouldBeEquivalentTo(expected);
         }
 
-        private RouteData CreateRouteData(string action, string controller, string area = null)
+        [Fact]
+        public void SaveRouteValues_UpdatesParams_WhenExistingRoute()
         {
-            return NavigationTestHelper.CreateRouteData(action, controller, area);
+            // Arrange ---------------------------
+
+            RouteData routeData = CreateRouteData("Index", "Tenants");
+
+            var initialParamsObject = new { p = 2, ps = 3 };
+            var updatedParamsObject = new { p = 5, ps = 3 };
+            var expectedObject = new { controller = "Tenants", action = "Index", p = 5, ps = 3 };
+
+            ISession session = new TestSession();
+
+            RouteValueDictionary expected = new RouteValueDictionary(expectedObject);
+
+            // Act -------------------------------
+
+            session.SaveRouteValues(routeData, new RouteValueDictionary(initialParamsObject));
+            session.SaveRouteValues(routeData, new RouteValueDictionary(updatedParamsObject));
+
+            RouteValueDictionary result = session.GetRouteValues(routeData);
+
+            // Assert ----------------------------
+
+            result.ShouldBeEquivalentTo(expected);
         }
+
+        private RouteData CreateRouteData(string action, string controller, string area = null) =>
+            NavigationTestHelper.CreateRouteData(action, controller, area);
     }
 }
